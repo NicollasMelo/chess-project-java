09a44932bf0550f2814a36068e12cf341d09bfb2
NicollasMelo <nicollasmelo2001@gmail.com>
2023-10-16 12:54:08 -0300

    Starting to implement board and piece

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import boardGame.Position;
+import boardGame.Board;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(10, 5);
-        System.out.print(position);
+
+        Board board = new Board(8,8);
     }
 }
